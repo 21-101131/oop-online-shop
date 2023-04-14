76fa11eca87cdf783e0943d643c080e101f46207
Abdullah Ayman <21-101131@students.eui.edu.eg>
2023-04-14 22:40:45 +0200

    added getProductList function

--- online_shop/src/com/oop/online_shop/DBConnection.java
+++ online_shop/src/com/oop/online_shop/DBConnection.java
@@ -1,5 +1,8 @@
 package com.oop.online_shop;
+
 import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DBConnection {
     private Connection con;
@@ -7,7 +10,7 @@ public class DBConnection {
     public void connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(
+            this.con = DriverManager.getConnection(
                     "jdbc:mysql://sql8.freemysqlhosting.net:3306/sql8612103",
                     "sql8612103",
                     "m624G5CJxr"
@@ -20,16 +23,36 @@ public class DBConnection {
         }
     }
 
+    protected List<Product> getProductList(){
+        List<Product> productList = new ArrayList<>();
+        try{
+            Statement stmt = this.con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from " + Product.tableName);
 
+            while(rs.next()) {
+                productList.add(
+                    new Product(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getDouble("price"),
+                        rs.getInt("quantity"),
+                        rs.getFloat("userRating")
+                    )
+                );
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
-    //Statement stmt=con.createStatement();
-   // ResultSet rs=stmt.executeQuery("select * from emp");
-	//		while(rs.next())
-   //                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+        return productList;
+    }
+
 
     public void closeConnection() {
         try {
-            con.close();
+            this.con.close();
             System.out.println("Connection Closed");
         } catch(Exception e) {
             e.printStackTrace();
--- online_shop/src/com/oop/online_shop/Product.java
+++ online_shop/src/com/oop/online_shop/Product.java
@@ -1,16 +1,17 @@
 package com.oop.online_shop;
 
-public class Product {
+public class Product extends DBConnection {
     private int id;
     private String name;
     private String description;
     private double price;
     private int quantity;
     private float userRating;
-    public Product(int id, String name, String descreiption, double price, int quantity, float userRating){
+    static String tableName = "Products";
+    public Product(int id, String name, String description, double price, int quantity, float userRating){
         this.id = id;
         this.name = name;
-        this.description = descreiption;
+        this.description = description;
         this.price = price;
         this.quantity = quantity;
         this.userRating = userRating;
