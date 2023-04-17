7c50697fadd5d86a4d42d997a503b7d8b2edcb61
Abdullah Ayman <21-101131@students.eui.edu.eg>
2023-04-17 21:39:05 +0200

    updates

--- cpp/onlineStore.h
+++ cpp/onlineStore.h
@@ -1,11 +1,60 @@
 #pragma once
 #ifndef ONLINESTORE_H
 #define ONLINESTORE_H
-#include "DB_functions.cpp"
+
+// #ifndef DB_FUNCTIONS_H
+// #define DB_FUNCTIONS_H
+
+#include <iostream>
+#include <windows.h>
+#include "mysql_connection.h"
+#include "cppconn/driver.h"
+#include "cppconn/exception.h"
+#include "cppconn/prepared_statement.h"
 #include <string>
 #include <vector>
 
 using namespace std;
+using namespace sql;
+
+// for demonstration only. never save your password in the code!
+//const string server = "sql8.freemysqlhosting.net";
+// const string username = "sql8613256";
+// const string password = "hwIw3Fjflu";
+
+class functions
+{
+    static Connection* con;
+
+public:
+    static void connectsql();
+    static void closeConnection();
+
+	template<typename T>
+    static vector<T> getProductList();
+
+protected:
+    static ResultSet* select(string tableName, string condition);
+    static ResultSet* getLastCreatedId(string tableName);
+
+	template<typename T>
+    static vector<T> getProductsInCart(int cartId);
+
+    static bool isProductInCart(int cartId, int productId);
+    static bool addProductInCart(int cartId, int productId, int count);
+    static bool removeFromCart(int cartId, int productId);
+
+	template<typename T>
+    static bool createUser(T user);
+
+	template<typename T>
+    static string savePayment(T payment);
+
+	template<typename R, typename U>
+    static R authenticateUser(U user);
+};
+
+//#endif // DB_FUNCTIONS_H
 
 class Product : public functions
 {
@@ -102,14 +151,6 @@ public:
 	string getCreditCardNumber();
 	// virtual Cart *getCart() = 0;
 };
-class UnRegisteredUser : public User
-{
-public:
-	UnRegisteredUser();
-	void Greeting() override;
-	RegisteredUser Login();
-	bool signUp();
-};
 
 class RegisteredUser : public User
 {
@@ -124,6 +165,16 @@ public:
 private:
 	Cart cart;
 };
+
+class UnRegisteredUser : public User
+{
+public:
+	UnRegisteredUser();
+	void Greeting() override;
+	RegisteredUser Login();
+	bool signUp();
+};
+
 class Payment : public functions
 {
 	int id;
